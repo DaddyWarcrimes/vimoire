008f88044a0931e23675fc59bd702ea049665ecc
DaddyWarcrimes <4444646+DaddyWarcrimes@users.noreply.github.com>
2023-05-17 06:27:50 -0500

    Tweaked conditional variable file creation; overwrite vimrc bindings

--- vimoire.sh
+++ vimoire.sh
@@ -1,14 +1,55 @@
-# Vimoir
-# An attempt at a vim and tmux based IDE in convenien bash scripts
-
-# Environment, things may happen if this is changed.
 #! /bin/bash
 
-PATH=$(realpath $1)
-echo $PATH
-if [ -e $PATH/.vimoire ]
+# Vimoir
+# An attempt at a vim and tmux based IDE in convenient bash scripts
+
+#----------------------------------------------------------------------------------------
+# User defined variables
+
+# Quick save keybinding
+
+QSKEY="<F11>"
+
+#----------------------------------------------------------------------------------------
+# Subroutines
+
+# Create vim variables file
+s_vars(){
+	echo "let vimoirepath = '$PPATH/.vimoire/quicksave.vim'" > $PPATH/.vimoire/vars.vim
+	echo "map $QSKEY :execute \"mksession! \" . vimoirepath<cr>" >> $PPATH/.vimoire/vars.vim
+	echo "inoremap $QSKEY <esc>:execute \"mksession! \" . vimoirepath<cr>" >> $PPATH/.vimoire/vars.vim
+}
+
+# 1st passed parameter is the path
+PPATH=$(realpath $1)
+#
+PNAME=$(basename $PPATH)
+
+
+if [ -e $PPATH/.vimoire ]
 then
-	echo 'found'
+	if [ -e $PPATH/.vimoire/vars.vim ]
+	then 
+		if [ -e $PPATH/.vimoire/quicksave.vim ]
+		then
+			vim -S $PPATH/.vimoire/quicksave.vim -S $PPATH/.vimoire/vars.vim $PPATH
+		else
+			vim -S $PPATH/.vimoire/vars.vim $PPATH
+		fi
+	else
+		if [ -e $PPATH/.vimoire/quicksave.vim ]
+		then
+			s_vars
+			vim -S $PPATH/.vimoire/quicksave.vim -S $PPATH/.vimoire/vars.vim $PPATH
+		else
+			s_vars
+			vim -S $PPATH/.vimoire/vars.vim $PPATH
+		fi
+	fi
+
 else
 	echo 'not found'
+	mkdir $PPATH/.vimoire
+	s_vars
+	vim -S $PPATH/.vimoire/vars.vim $PPATH
 fi
